f0e93c283459bccf8811dcf17952af58d021e3d4
Lev <lafitel31@gmail.com>
2024-09-23 18:44:20 +1000

    create views

--- AvtoInfo/users/views.py
+++ AvtoInfo/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from typing import TYPE_CHECKING
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from cars.models import Car
+
+if TYPE_CHECKING:
+    from django.http import HttpResponse, HttpRequest
+
+
+def login(request: "HttpRequest") -> "HttpResponse":
+    pass
+
+
+def registration(request: "HttpRequest") -> "HttpResponse":
+    pass
+
+
+@login_required
+def profile(request: "HttpRequest") -> "HttpResponse":
+    pass
+
+
+@login_required
+def logout(request: "HttpRequest") -> "HttpResponse":
+    pass
