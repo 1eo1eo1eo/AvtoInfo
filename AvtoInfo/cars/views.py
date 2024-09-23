b444be54c6af88ab1aada37e0b1cbf0f5c1e7693
Lev <lafitel31@gmail.com>
2024-09-23 18:17:18 +1000

    create func cars to show cars

---
+++ AvtoInfo/cars/views.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+from cars.models import Car, Comment
+
+if TYPE_CHECKING:
+    from django.http import HttpResponse, HttpRequest
+
+
+def cars(request: "HttpRequest") -> "HttpResponse":
+
+    page = request.GET.get("page", 1)
+    queryset = Car.objects.order_by("id")
+
+    context: dict = {
+        "title": "Cars - Home",
+        "cars": queryset,
+    }
+    return render(request, "cars/cars.html", context)
