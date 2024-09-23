574a1ec421fd76bf47c2c86ae557996b8a8a339b
Lev <lafitel31@gmail.com>
2024-09-23 18:47:28 +1000

    create User model

--- AvtoInfo/users/models.py
+++ AvtoInfo/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Meta:
+        db_table: str = "Users"
+        verbose_name: str = "User"
+        verbose_name_plural: str = "Users"
+
+    def __str__(self) -> str:
+        return self.username
