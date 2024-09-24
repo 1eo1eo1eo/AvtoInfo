f17ca0d1f3bd4ff593d4149da31d0f0bd0331c25
Lev <lafitel31@gmail.com>
2024-09-24 12:58:58 +1000

    create serializers for car and comment

---
+++ AvtoInfo/cars/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from cars.models import Car, Comment
+
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = [
+            "id",
+            "make",
+            "model",
+            "year",
+            "description",
+            "owner",
+            "created_at",
+            "updated_at",
+        ]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "content",
+            "car",
+            "author",
+            "created_at",
+        ]
