376fce6d4918d89df60f24bb94eb3baf92f5a991
Lev <lafitel31@gmail.com>
2024-09-24 12:59:53 +1000

    create get_cars, add_car, car_detail, edit_car, delete_car, api urls

--- AvtoInfo/cars/urls.py
+++ AvtoInfo/cars/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-from cars import views
 
 app_name = "cars"
 
+router = DefaultRouter()
+router.register(r"cars", views.CarViewSet)
 
 urlpatterns = [
-    path("", views.cars, name="home"),
+    path("", views.get_cars, name="home"),
+    path("add/", views.add_car, name="add_car"),
+    path("<int:car_id>/", views.car_detail, name="car_detail"),
+    path("<int:car_id>/edit/", views.edit_car, name="edit_car"),
+    path("<int:car_id>/delete/", views.delete_car, name="delete_car"),
+    path("api/", include(router.urls)),
 ]
