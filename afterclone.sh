e0b92499656929f77d645cf931b9be1c18a8bd6d
Lev <lafitel31@gmail.com>
2024-09-24 18:00:22 +1000

    change afterclone script

--- afterclone.sh
+++ afterclone.sh
@@ -1,15 +1,4 @@
+#!/bin/sh
+
+
 docker compose -f docker-compose-dev.yaml up -d --build
-
-poetry run pip install -r requirements.txt
-
-cd AvtoInfo
-
-poetry run python3 manage.py migrate
-
-poetry run python3 manage.py loaddata fixtures/users/users.json
-
-poetry run python3 manage.py loaddata fixtures/cars/cars.json
-
-poetry run python3 manage.py loaddata fixtures/cars/comments.json
-
-poetry run python3 manage.py runserver
