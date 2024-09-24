1c2caec40b5f975f686ce1dd4202faca00b4e3a6
Lev <lafitel31@gmail.com>
2024-09-24 17:12:41 +1000

    change host and add port

--- AvtoInfo/AvtoInfo/settings.py
+++ AvtoInfo/AvtoInfo/settings.py
@@ -89,7 +89,8 @@ DATABASES = {
         "NAME": "AvtoInfo",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "0.0.0.0",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
