c3fb486d6aaa0dc350d5202184d0a73135dbc806
Lev <lafitel31@gmail.com>
2024-09-24 13:00:43 +1000

    create viewset, get_cars, add_car, car_detail, edit_car, delete_car views

--- AvtoInfo/cars/views.py
+++ AvtoInfo/cars/views.py
@@ -1,15 +1,58 @@
 from typing import TYPE_CHECKING
 
-from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404, redirect
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from cars.serializers import CarSerializer, CommentSerializer
 from cars.models import Car, Comment
+from cars.forms import CarForm
 
 if TYPE_CHECKING:
-    from django.http import HttpResponse, HttpRequest
+    from django.http import HttpRequest, HttpResponse
 
 
-def cars(request: "HttpRequest") -> "HttpResponse":
+class CarViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с API.
+    """
+
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission = IsAuthenticatedOrReadOnly
+
+    @action(detail=True, methods=["get"])
+    def comments(self, request, pk=None):
+        car = self.get_object()
+        comments = Comment.objects.filter(car=car)
+        serializer = CommentSerializer(comments, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=True, methods=["post"])
+    def add_comment(self, request, pk=None):
+        car = self.get_object()
+        content = request.data.get("content")
+        comment = Comment.objects.create(
+            content=content,
+            car=car,
+            author=request.user,
+        )
+        serializer = CommentSerializer(comment)
+
+        return Response(serializer.data)
+
+
+def get_cars(request: "HttpRequest") -> "HttpResponse":
+    """
+    Представление для получения всех машин
+    """
 
     queryset = Car.objects.order_by("id")
 
@@ -18,3 +61,90 @@ def cars(request: "HttpRequest") -> "HttpResponse":
         "cars": queryset,
     }
     return render(request, "cars/cars.html", context)
+
+
+@login_required
+def add_car(request: "HttpRequest") -> "HttpResponse":
+    """
+    Представление для добавления новой машины.
+    """
+
+    if request.method == "POST":
+        form = CarForm(request.POST)
+        if form.is_valid():
+            car = form.save(commit=False)
+            car.owner = request.user
+            car.save()
+            return redirect(reverse("users:profile"))
+    else:
+        form = CarForm()
+
+    context = {
+        "title": "Добавить машину",
+        "form": form,
+    }
+
+    return render(request, "cars/add_car.html", context)
+
+
+def car_detail(request, car_id):
+    """
+    Представление с информацией о конкретном автомобиле и комментариями, а также форма для добавления комментариев
+    """
+
+    car = get_object_or_404(Car, id=car_id)
+    comments = Comment.objects.filter(car=car)
+
+    if request.method == "POST":
+        content = request.POST.get("content")
+        Comment.objects.create(
+            content=content,
+            car=car,
+            author=request.user,
+        )
+
+        return HttpResponseRedirect(reverse("cars:car_detail", args=[car_id]))
+
+    context = {
+        "car": car,
+        "comments": comments,
+    }
+
+    return render(request, "cars/car_detail.html", context)
+
+
+@login_required
+def edit_car(request, car_id):
+    """
+    Представление для изменеия данных машины
+    """
+
+    car = get_object_or_404(
+        Car,
+        id=car_id,
+        owner=request.user,
+    )
+
+    if request.method == "POST":
+        car.make = request.POST.get("make")
+        car.model = request.POST.get("model")
+        car.year = request.POST.get("year")
+        car.description = request.POST.get("description")
+        car.save()
+        return redirect(reverse("users:profile"))
+
+    context = {
+        "car": car,
+    }
+
+    return render(request, "cars/edit_car.html", context)
+
+
+@login_required
+def delete_car(request, car_id):
+    """
+    Представление для удаления машины
+    """
+    car = get_object_or_404(Car, id=car_id, owner=request.user)
+    car.delete()
+    return redirect(reverse("users:profile"))
